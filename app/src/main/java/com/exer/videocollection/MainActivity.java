e7d66809328059a80f529348f5a563a280ee5b5d
hehang0 <hehang724590957@qq.com>
2018-01-08 17:36:53 +0800

    添加项目

--- app/src/main/java/com/exer/videocollection/MainActivity.java
+++ app/src/main/java/com/exer/videocollection/MainActivity.java
@@ -16,7 +16,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
